b76c6c8118a50197ce4067ef1f17f47007aed3d7
Pritam Banerjee <pritam.uav@gmail.com>
2020-08-06 18:57:03 +0530

    checked to heroku components and modules created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherWidgetComponent } from './weather-widget/weather-widget.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherWidgetComponent
   ],
   imports: [
     BrowserModule,
